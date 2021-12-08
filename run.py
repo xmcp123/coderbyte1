1c4b08de3b1db755a81e619e0b6fa844f1d5817e
Michael Cojanu <mike@xmcp.live>
2021-12-07 16:43:02 -0800

    allow max pages as an argument

--- run.py
+++ run.py
@@ -13,9 +13,16 @@ if __name__ == "__main__":
         type=str,
         default="https://nearme.vip/restaurant/us/pizza-restaurant/square-pie-guys-of-san-francisco/",
     )
+
+    parser.add_argument(
+        "--max",
+        "-m",
+        type=int,
+        default=30,
+    )
     args = parser.parse_args()
 
-    job = ScraperJob(max_pages=20)
+    job = ScraperJob(max_pages=args.max)
     job.scrape(args.url)
     print(json.dumps(job.results, indent=3))
     print("Results: {}".format(len(job.results)))
